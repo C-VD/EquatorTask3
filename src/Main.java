4bb6f177847b494da97c289d6c8070de393e2aaf
C-VD <vldr@disroot.org>
2023-08-08 11:02:11 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("как тебя зовут?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        if (name.endsWith("а")
+         || name.endsWith("я")
+         || name.endsWith("и")
+        )
+        {
+            System.out.println("Приветик!");
+        }
+        else
+        {
+            System.out.println("Здорова!");
+        }
+    }
+}
